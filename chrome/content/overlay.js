2ddc5d60c6bfb828ea30c1243546131d3c81eec2
othree <othree@gmail.com>
2010-11-05 19:19:17 +0800

    now can upload to emma

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -5,6 +5,30 @@
  * using formdata and xhr: https://developer.mozilla.org/En/XMLHttpRequest/Using_XMLHttpRequest#Using_FormData_objects
  */
 
+function GetImageFromURL(url) { 
+  var ioserv = Components.classes["@mozilla.org/network/io-service;1"] 
+    .getService(Components.interfaces.nsIIOService); 
+  var channel = ioserv.newChannel(url, 0, null); 
+  var stream = channel.open(); 
+
+  if (channel instanceof Components.interfaces.nsIHttpChannel && channel.responseStatus != 200) { 
+    return ""; 
+  }
+
+  var bstream = Components.classes["@mozilla.org/binaryinputstream;1"] 
+    .createInstance(Components.interfaces.nsIBinaryInputStream); 
+  bstream.setInputStream(stream); 
+
+  var size = 0; 
+  var file_data = ""; 
+  while(size = bstream.available()) { 
+    file_data += bstream.readBytes(size); 
+  } 
+
+  return file_data; 
+}
+
+
 var Cc = Components.classes,
     Ci = Components.interfaces,
     Cr = Components.results,
@@ -263,7 +287,7 @@ var pixConn = {
             body += "Content-Disposition: form-data; name='upload_file'; filename='" + f.name + "'\r\n";  
             body += "Content-Type: " + f.type + "\r\n\r\n";
             body += f.getAsBinary() + "\r\n";
-            body += "--" + boundary + "--";
+            body += "--" + boundary + "--\r\n";
         }
 
         XHR.open(method, url, false);
@@ -281,8 +305,8 @@ var pixConn = {
         params = 'oauth_signature=' + encodeURIComponent(Base64.encode(signature));
         //XHR.send(params);
         if (f) {
-            //XHR.sendAsBinary(body);
-            XHR.send(body);
+            XHR.sendAsBinary(body);
+            //XHR.send(body);
         } else {
             XHR.send();
         }
